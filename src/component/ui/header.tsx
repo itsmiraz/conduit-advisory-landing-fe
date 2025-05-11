b622b282a6f5da41b8020dbe2e015af95c545122
itsmiraz <mirajulislamofficial044@gmail.com>
2025-05-11 11:17:56 +0600

    navbar done

--- src/component/ui/header.tsx
+++ src/component/ui/header.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from "react";
+import logo from "@/assets/images/logo.png";
 
 const Header = () => {
+  const navLinks = [
+    {
+      link: "/",
+      label: "Who Is This For?",
+    },
+    {
+      link: "/",
+      label: "Our Expertise",
+    },
+    {
+      link: "/",
+      label: "Why Choose Us?",
+    },
+    {
+      link: "/",
+      label: "Case Studies",
+    },
+  ];
 
   return (
-    <div>
-
-
+    <div className="max-w-[1220px] mt-[32px] mx-auto bg-[#D6BEFF14] rounded-[12px] border-[1px] border-[#FFFFFF14] p-[10px] font-sans flex justify-between items-center">
+     <div className="pl-2">
+       <img src={logo} alt="" />
+     </div>
+      <ul className="flex items-center gap-x-5">
+        {navLinks.map((item) => (
+          <li className="text-[16px] font-medium ">
+            <a className="cursor-pointer">{item.label}</a>
+          </li>
+        ))}
+      </ul>
+      <button className="buttonInnerShadow rounded-[10px] py-[10px] px-[28px]  font-semibold text-[16px] ">Book A Call</button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import RootLayout from "./component/layouts/rootLayout";
-
+import '@fontsource/inter/400.css'; // Regular weight (400)
+import '@fontsource/inter/700.css'; 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RootLayout>
