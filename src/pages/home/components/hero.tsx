85064f268a786d33b084860406a3c0620879af7b
itsmiraz <mirajulislamofficial044@gmail.com>
2025-09-12 19:18:41 +0600

    setup

---
+++ src/pages/home/components/hero.tsx
@@ -0,0 +1,20 @@
+import HeroImg from "@/assets/images/hero.webp";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="heroBg">
+        <p>Conduit Advisory</p>
+        <h1>The Conduit of Capital & Crypto Growth</h1>
+        <p>
+          We specialize in translating blockchain potential into
+          institutional-grade opportunities—offering deal structuring, token
+          strategy, and advisory services that align traditional finance with
+          the decentralized future.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./components/hero";
 
 const Home = () => {
-  return <div className="h-screen"></div>;
+  return (
+    <div className="h-screen">
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
