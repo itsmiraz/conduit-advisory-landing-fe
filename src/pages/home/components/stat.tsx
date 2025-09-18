32964c3d48256f4159e56c9e3bbcdf905e13b93b
itsmiraz <mirajulislamofficial044@gmail.com>
2025-09-18 09:25:59 +0600

    new changes

--- src/pages/home/components/stat.tsx
+++ src/pages/home/components/stat.tsx
@@ -99,7 +99,7 @@ const Stat = () => {
           </p>
         </div>
 
-        <div className="flex  lg:w-fit w-full md:flex-row flex-col gap-x-8">
+        <div className="hidden md:flex  lg:w-fit w-full md:flex-row flex-col gap-x-8">
           <div className="text-start md:block flex items-center gap-5 w-full lg:w-[286px]">
             <h2 className="text-primary font-bold text-[24px] md:text-[40px]">
               <CountUpNumber end={50} duration={1000} />+
@@ -125,6 +125,37 @@ const Stat = () => {
               <CountUpNumber end={100} duration={1100} />+
             </h2>
             <p className="text-[#2B2B2B] whitespace-nowrap md:whitespace-normal font-semibold text-sm md:text-xl">
+              Deal Structuring <br className="text-start md:block hidden" /> for
+              Crypto Startups
+            </p>
+          </div>
+        </div>
+        <div className="flex md:hidden  lg:w-fit w-full md:flex-row flex-col gap-x-8">
+          <div className="text-start  md:block grid grid-cols-5 items-center gap-5 w-full lg:w-[286px]">
+            <h2 className="text-primary font-bold col-span-1 text-[24px] md:text-[40px]">
+              <CountUpNumber end={50} duration={1000} />+
+            </h2>
+            <p className="text-[#2B2B2B] font-semibold pl-6 col-span-4 text-sm md:text-xl">
+              Token <br className="text-start md:block hidden" /> Generation
+              Event
+            </p>
+          </div>
+
+          <div className="text-start md:block grid grid-cols-5 items-center gap-5 w-full md:w-[286px] lg:w-[286px]">
+            <h2 className="text-primary col-span-1  font-bold text-[24px] md:text-[40px]">
+              <CountUpNumber end={200} duration={1200} />+
+            </h2>
+            <p className="text-[#2B2B2B] col-span-4 pl-6 font-semibold text-sm md:text-xl">
+              Corporate <br className="text-start md:block hidden" />{" "}
+              Organization
+            </p>
+          </div>
+
+          <div className="text-start md:block grid grid-cols-5  items-center gap-5 w-full lg:w-[286px]">
+            <h2 className="text-primary font-bold col-span-1 text-[24px] md:text-[40px]">
+              <CountUpNumber end={100} duration={1100} />+
+            </h2>
+            <p className="text-[#2B2B2B] col-span-4 pl-6 whitespace-normal md:whitespace-normal font-semibold text-sm md:text-xl">
               Deal Structuring <br className="text-start md:block hidden" /> for
               Crypto Startups
             </p>
